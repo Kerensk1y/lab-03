c63df5e3d715135d61d820ad7912797739c49eeb
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2021-06-17 22:09:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
-//using namespace std;
+#include "svg.h"
 
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
 vector<double>
-input_numbers(size_t count)
+input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cerr << "Ââåäèòå " << i + 1 << " ÷èñëî:\t";
-        cin >> result[i];
+        cerr << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ " << i + 1 << " Ñ‡Ð¸ÑÐ»Ð¾:\t";
+        in >> result[i];
     }
     return result;
 }
@@ -98,78 +98,18 @@ show_histogram_text(const vector<size_t> bins)
     }
 }
 
-void
-svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void
-svg_end()
-{
-    cout << "</svg>\n";
-}
-
-void
-svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
-}
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black" )
-{
-    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='"<< fill << "'/>";
-}
-
-void
-show_histogram_svg(const vector<size_t>& bins)
-{
-     size_t max_count = bins[0];
-
-    for (size_t i = 0; i < bins.size(); i++)
-    {
-        if (bins[i] > max_count)
-        {
-            max_count = bins[i];
-        }
-    }
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-    for (size_t bin : bins)
-    {
-        //const double bin_width = BLOCK_WIDTH * bin;
-        const double bin_width = IMAGE_WIDTH * bin / max_count;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "blue", "yellow");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
-}
-
 int main()
 {
     setlocale(LC_ALL, "RUS");
 
     size_t number_count;
-    cerr << "Ââåäèòå êîëè÷åñòâî ÷èëåë:\t";
+    cerr << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ñ‡Ð¸Ð»ÐµÐ»:\t";
     cin >> number_count;
 
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin, number_count);
 
     size_t bin_count;
-    cerr << "Ââåäèòå êîëè÷åñòâî ãðóïï:\t";
+    cerr << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð³Ñ€ÑƒÐ¿Ð¿:\t";
     cin >> bin_count;
 
 
