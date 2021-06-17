680e05772bc14cd322789026f790ebc6a1e5a9ee
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2021-06-17 22:59:27 +0300

    Update histogram.cpp

--- lab03-histogram/histogram.cpp
+++ lab03-histogram/histogram.cpp
@@ -1,6 +1,37 @@
 #include <vector>
 #include "histogram.h"
 
+const vector<size_t>
+make_histogram(Input input)
+{
+    double min;
+    double max;
+    vector<size_t> bins(input.bin_count);
+
+    find_minmax(input.numbers, min, max);
+
+    double bin_size = (max - min) / bins.size();
+    for (size_t i = 0; i < input.numbers.size(); i++)
+    {
+        bool found = false;
+        for (size_t j = 0; (j < bins.size() - 1) && !found; j++)
+        {
+            auto low = min + j * bin_size;
+            auto high = min + (j + 1) * bin_size;
+            if ((input.numbers[i] >= low) && (input.numbers[i] < high))
+            {
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            bins[bins.size() - 1]++;
+        }
+    }
+    return bins;
+}
+
 void
 find_minmax(const vector<double>& numbers, double& min, double& max)
 {
