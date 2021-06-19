7d541f986b02ca654723b01026d6894eba618b92
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2021-06-19 23:07:47 +0300

    Update main.cpp

--- lab03-histogram/main.cpp
+++ lab03-histogram/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
 
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
@@ -90,8 +91,27 @@ show_histogram_text(const vector<size_t> bins)
     }
 }
 
-int main()
+int
+main(int argc, char* argv[])
 {
+    if (argc > 1)
+    {
+        CURL* curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            /* ask libcurl to show us the verbose output */
+            // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+
+        }
+        return 0;
+    }
+
+    // curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const vector<size_t> bins = make_histogram(input);
     // show_histogram_text(bins);
